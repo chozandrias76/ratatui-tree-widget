5995f047be6f5049fcc42ab28e57b0b1dbf24eb8
woxjro <usuzawa.yoshi.55z@st.kyoto-u.ac.jp>
2023-08-20 12:41:40 +0900

    [feat]: use ratatui@0.22.0

--- examples/example.rs
+++ examples/example.rs
@@ -6,15 +6,15 @@ use crossterm::{
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
-use std::{error::Error, io};
-use tui::{
+use ratatui::{
     backend::{Backend, CrosstermBackend},
     style::{Color, Modifier, Style},
     widgets::{Block, Borders},
     Terminal,
 };
+use std::{error::Error, io};
 
-use tui_tree_widget::{Tree, TreeItem};
+use ratatui_tree_widget::{Tree, TreeItem};
 
 struct App<'a> {
     tree: StatefulTree<'a>,
